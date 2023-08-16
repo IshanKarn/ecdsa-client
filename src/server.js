b7bb6e24e5d01d5210841768dece1d4241774d33
Ishan <ishankarn14@gmail.com>
2023-08-16 14:31:52 +0530

    updated server cration for dev and prod env

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
-const server = axios.create({
+const prodServer = axios.create({
+  baseURL: "https://ecdsa-api.vercel.app/",
+});
+
+const devServer = axios.create({
   baseURL: "http://localhost:3045",
 });
+
+var server;
+
+if (process.env.NODE_ENV === 'production') {
+  server = prodServer;
+} else {
+  server = devServer;
+}
 
 export default server;
