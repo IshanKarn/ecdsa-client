69ad3b9585700d809b5ba74461e9743be7d67104
Ishan <ishankarn14@gmail.com>
2023-08-16 15:41:36 +0530

    updated server connection

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
 import axios from "axios";
 
-const prodServer = axios.create({
+// const prodServer = axios.create({
+//   baseURL: "https://ecdsa-api.vercel.app/",
+// });
+
+// const devServer = axios.create({
+//   baseURL: "http://localhost:3045",
+// });
+
+// var server;
+
+// if (process.env.NODE_ENV === 'production') {
+//   server = prodServer;
+// } else {
+//   server = devServer;
+// }
+
+const server = axios.create({
   baseURL: "https://ecdsa-api.vercel.app/",
 });
-
-const devServer = axios.create({
-  baseURL: "http://localhost:3045",
-});
-
-var server;
-
-if (process.env.NODE_ENV === 'production') {
-  server = prodServer;
-} else {
-  server = devServer;
-}
 
 export default server;
